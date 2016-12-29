75e2dd0c5ea1bff912eb02b734627c5eeedaa1d4
Felix Behr <felix.behr@spectos.com>
2016-12-29 09:17:46 +0100

    added keybindings for smerge

--- config/smerge-config.el
+++ config/smerge-config.el
@@ -1,4 +1,9 @@
 (use-package smerge-mode
-  :ensure t)
+  :ensure t
+  :config
+  (global-set-key (kbd "C-<f6>") 'smerge-prev)
+  (global-set-key (kbd "C-<f7>") 'smerge-keep-other)
+  (global-set-key (kbd "C-<f8>") 'smerge-keep-mine)
+  (global-set-key (kbd "C-<f9>") 'smerge-next))
 
 (provide 'smerge-config)
