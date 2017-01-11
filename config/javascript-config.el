0be609684a6962e641e8c960d888ee4dcb909813
Felix Behr <felix.behr@spectos.com>
2017-01-11 09:04:13 +0100

    don't prettify on save, use a keybinding instead

--- config/javascript-config.el
+++ config/javascript-config.el
@@ -7,12 +7,11 @@
 
 (use-package js2-mode
   :ensure t
+  :init
+  (bind-key "C-M-L" 'prettier)
   :config
   (setq indent-tabs-mode nil)
   (add-hook 'js2-mode-hook 'tern-mode)
-  (add-hook 'js2-mode-hook
-	    (lambda()
-	      (add-hook 'before-save-hook 'prettier-before-save)))
   (custom-set-variables
    '(js2-basic-offset 2)
    '(js2-bounce-indent-p t)
